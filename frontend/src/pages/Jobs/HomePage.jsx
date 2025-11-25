350825f23b69e8c0da5203798a34a4e91e3d8e33
Thijs Langerwerf <thijslangerwerf@gmail.com>
2025-11-25 21:38:09 +0100

    Afbeeldingen toegevvoegd. Oude jobs pagina's verwijderd.

---
+++ frontend/src/pages/Jobs/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from '../../components/Jobs/Hero';
+import HomeCards from '../../components/Jobs/HomeCards';
+import JobListings from '../../components/Jobs/JobListings';
+import ViewAllJobs from '../../components/Jobs/ViewAllJobs'
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true}/>
+      <ViewAllJobs />
+    </>
+  )
+}
+export default HomePage
